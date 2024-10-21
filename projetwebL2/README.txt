3ab7284b808c2bcd6caf37bf21159a1f2e5eb4ca
Ccestmoi <flo.bred77@gmail.com>
2024-10-21 21:55:30 +0200

    Add files via upload

--- projetwebL2/README.txt
+++ projetwebL2/README.txt
@@ -3,6 +3,8 @@
 Attributs et description de Questions
 
 id : identifiant unique pour trouver la question dans la bdd
+theme: thematique de la question
+niveau: difficulté de la question
 image : chemin relatif de l'image associée à la question
 contenu : la question sous forme de texte
 reponse : Les réponses possibles 4 au maximum
@@ -12,3 +14,11 @@ bonnereponse : Les numéro des bonnes reponses
 point :  points donnés si la réponse est correcte
 explication : explication de la réponse après validation des réponses
     de l'utilisateur
+
+
+Interface apprendre
+
+
+Bouton pour afficher toutes les questions
+Case thematique et niveau: permet le filtrage des questions en fonction 
+    du theme et niveau après click sur le bouton "appliquer"
--- projetwebL2/js/scripts.js
+++ projetwebL2/js/scripts.js
@@ -1,5 +1,9 @@
 let xmlhttp = new XMLHttpRequest();
 
+document.addEventListener("DOMContentLoaded", function() {
+    loadXMLDoc1();
+});
+
 function loadXMLDoc() {
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -27,4 +31,73 @@ function fetchData() {
         "</tr>";
     }
     document.getElementById("data").innerHTML = table;
-}     
+}
+
+function loadXMLDoc1() {
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            fetchData1();
+        }
+    };
+    xmlhttp.open("GET", "https://delightful-pond-089795303.4.azurestaticapps.net/projetwebL2/data/bdd.xml", true);
+    xmlhttp.send();
+}
+
+function fetchData1() {
+    let i;
+    let xmlDoc = xmlhttp.responseXML;
+    let table = "<tr><th>Question</th><th>Niveau</th><th>Points</th></tr>";
+    let x = xmlDoc.getElementsByTagName("question");
+    for (i = 0; i < x.length; i++) {
+        table += "<tr><td>" +
+        x[i].getElementsByTagName("contenu")[0].textContent +
+        "</td><td>" +
+        x[i].getElementsByTagName("niveau")[0].textContent +
+        "</td><td>" +
+        x[i].getElementsByTagName("point")[0].textContent +
+        "</td></tr>";
+    }
+    document.getElementById("data").innerHTML = table;
+}
+
+function filtrage() {
+    let theme = document.getElementById("theme");
+    let niveau = document.getElementById("niveau");
+
+    let choix_theme = theme.value;
+    let choix_niveau = niveau.value;
+
+    // Construction d'une nouvelle table
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            let i;
+            let xmlDoc = xmlhttp.responseXML;
+            let table = "<tr><th>Question</th><th>Niveau</th><th>Points</th></tr>";
+            let x = xmlDoc.getElementsByTagName("question");
+            for (i = 0; i < x.length; i++) {
+                let questionTheme = x[i].getElementsByTagName("theme")[0].textContent;
+                let questionNiveau = x[i].getElementsByTagName("niveau")[0].textContent;
+
+                if ((choix_theme === "" || questionTheme == choix_theme) && 
+                    (choix_niveau === "" || questionNiveau == choix_niveau)) {
+                    table += "<tr><td>" +
+                    x[i].getElementsByTagName("contenu")[0].textContent + 
+                    "</td><td>" +
+                    questionNiveau +
+                    "</td><td>" +
+                    x[i].getElementsByTagName("point")[0].textContent +
+                    "</td></tr>";
+                }
+            }
+            document.getElementById("data").innerHTML = table;
+        }
+    };
+    xmlhttp.open("GET", "https://delightful-pond-089795303.4.azurestaticapps.net/projetwebL2/data/bdd.xml", true);
+    xmlhttp.send();  
+}
+
+function reset() {
+    document.getElementById("theme").value = "";
+    document.getElementById("niveau").value = "";
+    loadXMLDoc1();
+}
