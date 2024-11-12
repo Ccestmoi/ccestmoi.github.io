b3ff2380c2afc2366171dded036f3d38cc23f9f1
Ccestmoi <flo.bred77@gmail.com>
2024-11-12 19:13:15 +0100

    Add files via upload

--- projetwebL2/js/scripts.js
+++ projetwebL2/js/scripts.js
@@ -13,6 +13,10 @@ let UserAnswers = [];
 let niveau = 1;
 let selection = 0;
 
+//Définiton de la variable de temps
+let time = 0;
+let timerInterval;
+
 //Définition des variables pour les statistiques
 
 
@@ -24,22 +28,15 @@ let startIndex = 0;
 let endIndex = 0;
 let page = 1;
 
-//Charge la bdd pour la page apprendre
+//Charge la bdd
 function loadXMLDoc1() {
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            fetchData();
-        }
-    };
-    xmlhttp.open("GET", "https://delightful-pond-089795303.4.azurestaticapps.net/projetwebL2/data/bdd.xml", true);
-    xmlhttp.send();
-}
-
-//Charge la bdd pour la page detail
-function loadXMLDocDisplay() {
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            displayQuestionById();
+            if (window.location.pathname.includes("detail.html")) {
+                displayQuestionById();
+            } else {
+                fetchData();
+            }
         }
     };
     xmlhttp.open("GET", "https://delightful-pond-089795303.4.azurestaticapps.net/projetwebL2/data/bdd.xml", true);
@@ -92,9 +89,11 @@ function displayData() {
 
 //Filtrage de la table dans la page apprendre
 function filtrage() {
+    //Récupérer les valeurs des champs de filtrage
     let theme = document.getElementById("theme");
     let niveau = document.getElementById("niveau");
 
+    //Filtrer les données
     let choix_theme = theme.value;
     let choix_niveau = niveau.value;
 
@@ -109,7 +108,9 @@ function filtrage() {
             data.push(x[i]);
         }
     }
-    displayData();
+
+    // Trier les données si un tri est sélectionné
+    sortData();
 }
 
 //Bouton pour afficher tout le tableau dans la page apprendre
@@ -131,9 +132,12 @@ function triPoints(a, b) {
     return 0;
 }
 
+//Fonction pour trier les données
 function sortData() {
     let n = document.getElementById("sort").value;
-    if (n === "Pasc"){
+    if (n === "") {
+        return;
+    } else if (n === "Pasc"){
         data.sort(triPoints);
     }
     else if (n === "Pdesc") {
@@ -166,7 +170,6 @@ function showPageLinks() {
 
 //Appel de la fonction dans la page detail
 function displayQuestionById() {    
-
     let questionid;
     
     //Récupérer id  dans la chaîne de requête
@@ -195,6 +198,7 @@ function displayQuestionById() {
 
 //Fonction de choix de niveau
 function Niveau(n) {
+    //Définition du niveau
     if (n == "1") {
         document.getElementById("validation").style.display = "block";
         niveau = 1;
@@ -206,14 +210,17 @@ function Niveau(n) {
         niveau = 3;
         time = 10;
     }
-    console.log(niveau);
+
+    //Affichage des divisions du jeu
     document.getElementById("niveau").style.display = "none";
+    document.getElementById("timer").style.display = "flex";
     document.getElementById("question").style.display = "flex";
     startjeu();
 }
 
 //Fonction pour démarrer le jeu
 function startjeu(){
+    //Récupérer les paramètres de l'URL
     const urlParams = new URLSearchParams(window.location.search);
     const theme = urlParams.get('theme');
     const lot = urlParams.get('lot');
@@ -242,6 +249,18 @@ function startjeu(){
     displayQuestion();
 };
 
+function timer() {
+    timerInterval = setInterval(function() {
+        time--;
+        if (time > -1) {
+            document.getElementById("timer-value").innerHTML = time;
+        } else {
+            clearInterval(timerInterval); // Arrêt du timer
+            displayQuestion();
+        }
+    }, 1000);
+}
+
 //Fonction pour afficher les questions dans le jeu
 function displayQuestion() {
     if (currentQuestion >= questions_jeu.length - 1) {
@@ -249,6 +268,17 @@ function displayQuestion() {
         return;
     }
     currentQuestion++;
+
+    //Timer
+    if (niveau == 1) {
+        time = 31;
+    } else if (niveau == 2) {
+        time = 21;
+    } else if (niveau == 3) {
+        time = 6;
+    }
+    timer();
+
     let question = questions_jeu[currentQuestion];
     document.getElementById("question").innerHTML = "<h4>Question</h4><br>" + question.getElementsByTagName("contenu")[0].textContent;
     document.getElementById("reponses-choix").innerHTML = "<br><h4>Reponses</h4>";
@@ -261,6 +291,8 @@ function displayQuestion() {
             document.getElementById("reponses-choix").innerHTML += "<button type='button' class='btn btn-outline-primary' onclick='selectionReponse(" + j + ")'>" + reponse + "</button>";
         }
     }
+
+    
 }
 
 // Fonction selection de la réponse
@@ -283,15 +315,20 @@ function validateAnswer() {
 
 //Fonction pour vérifier la réponse
 function checkAnswer(reponse) {
+    clearInterval(timerInterval);
+
     let question = questions_jeu[currentQuestion];
     let reponseCorrecte = question.getElementsByTagName("bonnereponse")[0].textContent;
     UserAnswers.push(reponse);
     if (reponse == reponseCorrecte) {
-        document.body.style.backgroundColor = "rgb(0, 255, 0, 0.5)";
+        if (niveau != 3) {
+            document.body.style.backgroundColor = "rgb(0, 255, 0, 0.5)";
+        }
         score += parseInt(question.getElementsByTagName("point")[0].textContent);
     } else {
-        document.body.style.backgroundColor = "rgb(255, 0, 0, 0.5)";
-        //alert("Mauvaise réponse !");
+        if (niveau != 3) {
+            document.body.style.backgroundColor = "rgb(255, 0, 0, 0.5)";
+        }
     }
     let fchaine = Array.from(UserAnswers).join(',');
     localStorage.setItem("reponses", fchaine);
@@ -299,22 +336,36 @@ function checkAnswer(reponse) {
     setTimeout(function() {document.body.style.backgroundColor = "";displayQuestion()}, 1000);
 }
 
+// ! Le tableau ne s'affiche pas si fchaine est vide
+
+
 //Fonction de création du tableau de correction
 function correction() {
+    //Récupération des réponses utilisateur
     let fchaine = localStorage.getItem("reponses");
     let reponses = fchaine.split(',');
-    console.log(reponses);
+
+    //Création du tableau
     let table = "<tr><th>Question</th><th>Réponse correcte</th><th>Réponse donnée</th></tr>";
     for (let i = 0; i < questions_jeu.length; i++) {
         let question = questions_jeu[i];
         let reponseCorrecte = question.getElementsByTagName("bonnereponse")[0].textContent;
-        console.log(reponses[i]);
+
+        //Gestion des réponses utilisateur
+        let ReponseUser = reponses[i];
+        if (ReponseUser == undefined) {
+            ReponseUser = "Pas de réponse";
+        } else {
+            ReponseUser = question.getElementsByTagName("reponse" + ReponseUser)[0].textContent;
+        }
+
+        //Création du tableau
         table += "<tr><td>" +
         question.getElementsByTagName("contenu")[0].textContent +
         "</td><td>" +
         question.getElementsByTagName("reponse" + reponseCorrecte)[0].textContent +
         "</td><td>" +
-        question.getElementsByTagName("reponse" + reponses[i])[0].textContent +
+        ReponseUser +
         "</td></tr>";
     }
     document.getElementById("table-correction").innerHTML = table;
@@ -325,6 +376,7 @@ function endGame() {
     correction();
     document.getElementById("question").style.display = "none";
     document.getElementById("choix").style.display = "none";
+    document.getElementById("timer").style.display = "none";
     document.getElementById("score").innerHTML += score + "/" + maxscore;
     document.getElementById("score").style.display = "flex";
     document.getElementById("table-correction").style.display = "flex";
